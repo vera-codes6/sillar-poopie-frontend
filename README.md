18991888c4a6d146a055db2fec4bb93f45ecec03
Sillar-codes <sillarcode@gmail.com>
2025-05-04 22:58:27 -0700

    fix app.tsx

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-## My name is Sillar.
+# Poopie Frontend using React
+
+Azure URL: https://victorious-pond-0b1a2190f.6.azurestaticapps.net
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { CssBaseline } from '@mui/material'
 import AppTheme from '@/theme/AppTheme'
-import { RouterProvider } from 'react-router-dom'
+import { RouterProvider, useNavigate } from 'react-router-dom'
 import router from './routes'
 import { ToastContainer } from 'react-toastify'
 import { useEffect } from 'react'
 import { connectSocket } from './lib/socket'
 import { useIsLoggedIn } from './hooks/useIsLoggedIn'
+import { ROUTES } from './constants/routes'
 
 export default function App() {
   const isLoggedIn = useIsLoggedIn()
+  const navigate = useNavigate()
+
+  if (isLoggedIn) navigate(ROUTES.DASHBOARD.HOME)
 
   const init = async () => {
     if (isLoggedIn) await connectSocket()
